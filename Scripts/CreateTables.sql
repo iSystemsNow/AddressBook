53bea693ff1270a4ac74ebb526ab93b64633cd62
fli-isystemsnow <fli@isystemsnow.com>
2019-05-06 21:20:36 -0400

    Inserting country and provincestate

--- Scripts/CreateTables.sql
+++ Scripts/CreateTables.sql
@@ -47,7 +47,7 @@ CREATE TABLE [dbo].[ProvinceState] (
 CREATE TABLE [dbo].[Country] (
     [Id] INT IDENTITY (1, 1) NOT NULL,
     [Name] VARCHAR (255) NOT NULL,
-    [Code] INT NOT NULL
+    [Code] CHAR(3)  NOT NULL
 	PRIMARY KEY (Id)
 );
 
@@ -62,3 +62,76 @@ IF OBJECT_ID('dbo.Contact_H', 'U') IS NOT NULL DROP TABLE [dbo].[Contact_H];
 
 SELECT * INTO [dbo].[Contact_H] FROM [dbo].[Contact] WHERE 1 = 2
 SELECT * INTO [dbo].[Address_H] FROM [dbo].[Address] WHERE 1 = 2
+
+INSERT INTO [dbo].[Country]([Name], [Code])
+VALUES ('United States','USA'),
+		('Canada','CA');
+
+INSERT INTO [dbo].[ProvinceState]([Name], [Code],[CountryId]) VALUES
+('Alabama, US','AL',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Alaska, US','AK',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Arizona, US','AZ',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Arkansas, US','AR',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Armed Forces America','AA',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Armed Forces Europe','AE',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Armed Forces Pacific','AP',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('California, US','CA',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Colorado, US','CO',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Connecticut, US','CT',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Delaware, US','DE',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('District of Columbia, US','DC',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Florida, US','FL',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Georgia, US','GA',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Hawaii, US','HI',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Idaho, US','ID',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Illinois, US','IL',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Indiana, US','IN',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Iowa, US','IA',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Kansas, US','KS',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Kentucky, US','KY',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Louisiana, US','LA',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Maine, US','ME',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Maryland, US','MD',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Massachusetts, US','MA',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Michigan, US','MI',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Minnesota, US','MN',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Mississippi, US','MS',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Missouri, US','MO',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Montana, US','MT',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Nebraska, US','NE',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Nevada, US','NV',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('New Hampshire, US','NH',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('New Jersey, US','NJ',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('New Mexico, US','NM',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('New York, US','NY',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('North Carolina, US','NC',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('North Dakota, US','ND',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Ohio, US','OH',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Oklahoma, US','OK',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Oregon, US','OR',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Pennsylvania, US','PA',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Rhode Island, US','RI',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('South Carolina, US','SC',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('South Dakota, US','SD',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Tennessee, US','TN',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Texas, US','TX',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Utah, US','UT',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Vermont, US','VT',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Virginia, US','VA',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Washington, US','WA',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('West Virginia, US','WV',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Wisconsin, US','WI',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Wyoming, US','WY',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'United States')),
+('Alberta, CA','AB',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'Canada')),
+('British Columbia, CA','BC',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'Canada')),
+('Manitoba, CA','MB',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'Canada')),
+('New Brunswick, CA','NB',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'Canada')),
+('Newfoundland and Labrador, CA','NL',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'Canada')),
+('Northwest Territories, CA','NT',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'Canada')),
+('Nova Scotia, CA','NS',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'Canada')),
+('Nunavut, CA','NU',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'Canada')),
+('Ontario, CA','ON',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'Canada')),
+('Prince Edward Island, CA','PE',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'Canada')),
+('Quebec, CA','QC',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'Canada')),
+('Saskatchewan, CA','SK',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'Canada')),
+('Yukon, CA','YT',(SELECT  Id FROM [dbo].[Country] WHERE [Name] = 'Canada'))
